d297e973b20413fd3594dfa8e0c5faa5f1baa413
Delfin Hernandez Gomez <04853767P@externos.santalucia.es>
2020-11-16 19:46:22 +0100

    Update protocol

--- Calculadora/ViewController.swift
+++ Calculadora/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CalculadoraController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,5 +27,45 @@ protocol CalculadoraProtocol {
     func modulo(num1: Int)-> Float
     func isOdd(num1: Int)-> Bool
 
+}
+
+
+extension CalculadoraController: CalculadoraProtocol {
+    
+    func suma(num1: Int, num2: Int) -> Int {
+        
+        return 0
+    }
+    
+    func resta(num1: Int, num2: Int) -> Int {
+        
+        return 0
+
+    }
+    
+    func division(num1: Int, num2: Int) -> Float {
+        
+        return 0
+
+    }
+    
+    func multiplicacion(num1: Int, num2: Int) -> Float {
+        
+        return 0
+
+    }
+    
+    func modulo(num1: Int) -> Float {
+        
+        return 0
+
+    }
+    
+    func isOdd(num1: Int) -> Bool {
+        
+        return true
+    }
+    
+    
 }
 
